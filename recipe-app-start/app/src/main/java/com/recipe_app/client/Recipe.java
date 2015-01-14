1318de616d9bc90635ee3085a7e903e642aacc5f
Shawn Simister <simister@google.com>
2015-01-14 12:19:38 -0800

    Clean up recipe-app-start
    Remove unused code and resources

--- recipe-app-start/app/src/main/java/com/recipe_app/client/Recipe.java
+++ recipe-app-start/app/src/main/java/com/recipe_app/client/Recipe.java
@@ -17,24 +17,11 @@
 package com.recipe_app.client;
 
 import android.database.Cursor;
-import android.util.Log;
 
 import com.recipe_app.client.database.RecipeIngredientTable;
 import com.recipe_app.client.database.RecipeInstructionsTable;
 import com.recipe_app.client.database.RecipeTable;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
